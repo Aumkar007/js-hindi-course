f2a9ac1c9b95a09f7170017605d8a30a90741b1b
Aumkar007 <53569373+Aumkar007@users.noreply.github.com>
2024-07-26 17:17:34 +0000

    container config done

---
+++ basics_01/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1445
+let accountEmail = "aumkar@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+accountEmail = "akm@gmail.com"
+accountPassword = "54321"
+accountCity = "Pune"
+let accountState = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])

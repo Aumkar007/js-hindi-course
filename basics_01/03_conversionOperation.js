a1f8276c7ca000c3f2c2ffa8e298668958bffc5d
Aumkar007 <53569373+Aumkar007@users.noreply.github.com>
2024-07-27 16:42:17 +0000

    conversion of datatypes

---
+++ basics_01/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score  = null
+//console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn  = "Hitesh"
+ 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
